b6195ed25fe721d484799662913a2268a2f9cb09
jaxtonmaez <54608481+jaxtonmaez@users.noreply.github.com>
2019-11-10 01:23:51 -0700

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,25 @@
+/*
+* Jaxton Maez
+* CS 1410
+* 11/10/19
+*/
+
+import java.security.SecureRandom;
+
+public class Driver
+{
+  public static void main(String[] args)
+  {
+    double aPos[][] = {{1,2},{1,1}};
+    Object a = new Object("Mars", aPos);
+    
+    a.printPosition();
+    
+    double arr[][] = a.getPosition();
+   // System.out.println(arr);
+    
+    
+    
+  }
+}
+
