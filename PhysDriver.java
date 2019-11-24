03e13c2292ac91c69d9b154335497f903ab2426e
jaxtonmaez <54608481+jaxtonmaez@users.noreply.github.com>
2019-11-24 01:59:38 -0700

    Update PhysDriver.java

--- PhysDriver.java
+++ PhysDriver.java
@@ -5,7 +5,8 @@
 */
 public class PhysDriver{
 	
-	public static double bigG = 6.67*Math.pow(10, -11);
+	//public static double bigG = 6.67*Math.pow(10, -11);
+    public int time = 0;
 	
 	
 
@@ -15,23 +16,35 @@ public class PhysDriver{
 		
 		Space sp = new Space(50,50);    //A square region of space with sidelegth 50;
 		
-		double[] obVeloc = {0,0};         //A velocity vector to send to Object.
-		Object ob = new Object("Earth", 999999, obVeloc , sp, 10, 10);   //create an object
+		double[] obVeloc = {1,1};         //A velocity vector to send to Object.
+		Object ob = new Planet("Earth", Integer.MAX_VALUE, obVeloc , sp, 30, 18);   //create an object
 		
-		double[] ob2Veloc = {1,0};
-		Object ob2 = new Object("Moon", 999999, obVeloc, sp, 0, 0);
+		double[] ob2Veloc = {1,1};
+		Object ob2 = new Planet("Moon", Integer.MAX_VALUE/2, ob2Veloc, sp, 1, 1);
 		
 		Interact(ob, ob2);
 		
 	}
 	
 	public static void Interact(Object ob, Object ob2)
-	{
+    {
 		double a = Space.Distance(ob, ob2);
-		double b = Object.Gravity(ob, ob2);
+		double[] b = Space.Gravity(ob, ob2);
 		System.out.println("The distance: "+a);
-		System.out.println("The gravity: " +b);
-		
+        for(int i=0; i<=1; i++)
+        {System.out.println("Ob1 gravity: " +b[i]);}
+        for(int j=2; j<=3; j++)
+        {System.out.println("ob2 gravity :"+b[j]);}
+        
+       // double[] arr = Space.Move1(ob, ob2);
+       // for(int i=0; i<=1; i++)
+       //     System.out.println("object1 moves by: "+arr[i]);
+       // double[] arr2 = Space.Move2(ob, ob2);
+       //for(int i=0; i<=1; i++)
+        //    System.out.println("object2 moves by: "+arr2[i]);
+        
+        
+        
 		
 	}
 
