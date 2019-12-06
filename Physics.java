380dce13d49981aaa80c373815a07a0005bc1100
jaxtonmaez <54608481+jaxtonmaez@users.noreply.github.com>
2019-12-05 17:25:38 -0700

    Update Physics.java

--- Physics.java
+++ Physics.java
@@ -15,6 +15,7 @@ public class Physics extends JPanel{
     public static int PrintInterval = 3600;      //ask user for time intervals to print data. default once an hour
     public static String data = null;           //create empty string that will be written into data.txt
     public static String data2 = null;
+    public static String data3 = null;
     public static Space sp = new Space(10000,10000);    //A region of space with size 10000,10000;
 	public static double maxDistance = Math.pow(Math.pow(sp.getHeight(),2)+Math.pow(sp.getWidth(), 2),0.5); //max distance of two objects can be apart.  
 
@@ -23,15 +24,17 @@ public class Physics extends JPanel{
 		time = 0;
 	    BufferedWriter writer = new BufferedWriter(new FileWriter("data.txt"));  //make a writer for readable data.txt
 	    BufferedWriter writer2 = new BufferedWriter(new FileWriter("MathematicaData.txt"));
+	    BufferedWriter writer3 = new BufferedWriter(new FileWriter("MathematicaData2.txt"));
 			
 		do{
 			Interact(ob, ob2);              //send objects to interact, until they collide or leave region
 		    if(time%PrintInterval == 0) {
 			writer.write(data);             //write data to file
 			writer2.write(data2);
+			writer3.write(data3);
 		    }
 		}
-		while(Space.Distance(ob, ob2)>=1 && Space.Distance(ob, ob2)<=maxDistance && time<=180000);
+		while(Space.Distance(ob, ob2)>=1 && Space.Distance(ob, ob2)<=maxDistance && Space.Distance(ob, ob2)>0 && time<=180000);
         		
 		if(Space.Distance(ob, ob2)<=1)
 			{System.out.println("\nObjects have collided after "+time+" seconds, which is about "+time/60/60+" hours.");    //print different statements 
@@ -47,6 +50,7 @@ public class Physics extends JPanel{
 			writer.write(data);}
 		writer.close();
 		writer2.close();
+		writer3.close();
 		return data;
 	}
 		
@@ -68,11 +72,12 @@ public class Physics extends JPanel{
   
         data = "\nDistance: "+distance+"\nTime: "+time+" seconds \nObject One: \t X gravity: "
         +gravity[0]+"\t X motion: "+move[0]+"\t X velocity: "+delatVeloc[0]
-        +"\n\t\t\t\t Y gravity: " +gravity[1]+"\t Y motion: "+move[1]+"\t Y velocity: "+delatVeloc[1]
+        +"\n\t\t Y gravity: " +gravity[1]+"\t Y motion: "+move[1]+"\t Y velocity: "+delatVeloc[1]
         +"\nObject Two: \t X gravity: " +gravity[2]+"\t X motion: "+move[2]+"\t X velocity: "+delatVeloc[2]
-        +"\n\t\t\t\t Y gravity: " +gravity[3]+"\t Y motion: "+move[3]+"\t Y velocity: "+delatVeloc[3];           //put information into String data
+        +"\n\t\t Y gravity: " +gravity[3]+"\t Y motion: "+move[3]+"\t Y velocity: "+delatVeloc[3];           //put information into String data
         
-        data2 =+ob.getX()+", "+ob.getY()+"\n"; 
+        data2 =+ob.getX()+", "+ob.getY()+", "+time+"\n"; 
+        data3 =+ob2.getX()+", "+ob2.getY()+", "+time+"\n";  
 	}
 	public static void setPrintInterval(int Interval) {Physics.PrintInterval = Interval;}
 
